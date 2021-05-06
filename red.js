ba7bd0216da0f32607a7a03e051d7800f515e661
NKS-png <81745470+NKS-png@users.noreply.github.com>
2021-05-06 12:36:55 +0530

    Add files via upload

---
+++ red.js
@@ -0,0 +1,21 @@
+class red {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.5,
+         
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+     
+      rectMode(CENTER);
+      fill("red")
+      rect(pos.x, pos.y, this.width, this.height);
+      
+    }
+  };
